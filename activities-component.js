0ea77e38cd0806209b1b382e8e3933a663d455fb
patrickmhitti <46557063+patrickmhitti@users.noreply.github.com>
2020-02-29 23:05:20 +0200

    Add files via upload

---
+++ activities-component.js
@@ -0,0 +1,89 @@
+
+
+var activities = Vue.extend({
+    data () {
+        return {
+          lines: null,
+          stopPoints: null,
+          shortSchedule: null,
+          longScheduleInbound: null,
+          longScheduleOutbound: null,
+          key: '',
+          selectedStation: null,
+          timer: null,
+          linesDisruptions: null,
+          lastUpdateDate: null,
+          selectedLine: null
+        }
+      },
+    template: '#activities-template',
+  
+    methods: {
+        formatDate(value) {
+            if (value) {
+              return moment(String(value)).format('MM/DD/YYYY hh:mm:ss')
+            }
+          },
+        compare(a, b) {
+            if (a.expectedArrival - b.expectedArrival) return 1;
+            if (b.expectedArrival > a.expectedArrival) return -1;
+          
+            return 0;
+          },
+        onSelectLine(event){
+          axios
+          .get('https://api.tfl.gov.uk/Line/' + event.target.value + '/StopPoints')
+          .then(response => {(this.stopPoints = response.data);});
+          document.getElementById("cmbStations").focus();
+        },
+        onSelectStation(event){
+            if (event == undefined)
+            {
+                event = selectedStation;
+            }
+            else
+            {
+                selectedStation = event;
+            }
+            clearTimeout(this.timer); 
+            this.timer = setInterval(this.onSelectStation, 6000)
+            this.selectedLine = document.getElementById("cmbLines").value;
+            console.log(this.selectedLine);
+            axios
+            .get('https://api.tfl.gov.uk/Line/' + this.selectedLine + '/Arrivals/' + event.target.value)// + '?direction=inbound')
+            .then(response => {
+                this.shortSchedule = response.data.sort(this.compare);  
+
+                this.longScheduleInbound = response.data.sort(this.compare);
+                this.longScheduleInbound = this.longScheduleInbound.slice().filter(a => a.direction == 'inbound');
+
+                this.longScheduleOutbound = response.data.sort(this.compare);
+                this.longScheduleOutbound = this.longScheduleOutbound.slice().filter(a => a.direction == 'outbound');   
+                this.lastUpdateDate = new Date();
+                console.log('Refresh completed at: ' + this.formatDate(this.lastUpdateDate))
+            }); 
+        }
+      },
+      mounted () { 
+        axios
+          .get('https://api.tfl.gov.uk/Line/Mode/tube')
+          .then(response => (this.lines = response.data));
+
+          axios
+            .get('https://api.tfl.gov.uk/Line/Mode/tube/Disruption') //https://api.tfl.gov.uk/Line/Mode/tube%2C%20bus/Disruption
+            .then(response => {
+                this.linesDisruptions = response.data.sort(this.compare);  
+            })
+      }
+});
+Vue.component('activities-component', activities);
+
+new Vue({
+    el: '#app',
+    data: {
+        todos: [{
+            title: 'Todo 1',
+            completed: false
+        }]
+    }
+});
